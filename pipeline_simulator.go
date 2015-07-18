394d51d864feb50fe4c09281cefa4d9282d84ee6
Oliver Nowak <oliver@olivernowak.com>
2015-07-18 11:44:15 -0400

    + added dump funcs

--- pipeline_simulator.go
+++ pipeline_simulator.go
@@ -20,6 +20,8 @@ const (
 var Main_Mem = make([]byte, MAX_MEMORY)
 var Regs = make([]int, MAX_REGS)
 var clock_cyle = 0
+var pc = 0
+var exit_requested = false
 
 ///////////////////
 // Instructions //
@@ -31,6 +33,7 @@ var instructions = []int{
 // 0xad09fffc,
 // 0x00625022}
 
+// TODO: swap for assignment instructions
 // var instructions = []int{
 // 	0xa1020000,
 // 	0x810AFFFC,
@@ -48,6 +51,7 @@ var instructions = []int{
 var ifid_w = new(IF_ID_Write)
 var ifid_r = new(IF_ID_Read)
 
+// TODO: remove inst codes not requested by assignment parameters
 var func_codes = map[int]string{
 	0x0:  "nop",
 	0x20: "add",
@@ -66,12 +70,12 @@ type Base_Inst struct {
 }
 
 type R_Inst struct {
-	Base_Inst
+	Base_Inst         // anonymous base type
 	funct, rd, rs, rt int
 }
 
 type I_Inst struct {
-	Base_Inst
+	Base_Inst  // anonymous base type
 	op, rt, rs int
 	offset     int16
 }
@@ -93,12 +97,13 @@ type IF_ID_Read struct {
 }
 
 func (r *IF_ID_Base) dump_IF_ID() {
+	fmt.Printf("\n")
 	fmt.Printf("IF / ID %s \n", r.Reg_Type)
 	fmt.Println("------------")
 	fmt.Printf("Inst = %.8X     [%s]     IncrPC = %d \n", r.Instruction, r.Instruction_String, r.Incr_PC)
 }
 
-type ID_EX_Write struct {
+type ID_EX_Base struct {
 	RegDst         bool
 	ALUSrc         bool
 	ALUOp          bool
@@ -114,66 +119,95 @@ type ID_EX_Write struct {
 	WriteReg_20_16 int
 	WriteReg_15_11 int
 	Function       int
+	Reg_Type       string
+	Instr_String   string
+}
+
+type ID_EX_Write struct {
+	ID_EX_Base
 }
 
 type ID_EX_Read struct {
-	RegDst         bool
-	ALUSrc         bool
-	ALUOp          bool
-	MemRead        bool
-	MemWrite       bool
-	Branch         bool
-	MemToReg       bool
-	RegWrite       bool
-	Incr_PC        int
-	ReadReg1Value  int
-	ReadReg2Value  int
-	SEOffset       int
-	WriteReg_20_16 int
-	WriteReg_15_11 int
-	Function       int
+	ID_EX_Base
+}
+
+func (r *ID_EX_Base) dump_ID_EX() {
+	fmt.Printf("\n")
+	fmt.Printf("ID / EX %s \n", r.Reg_Type)
+	fmt.Println("------------")
+
+	fmt.Printf("Control: \n")
+	fmt.Printf("RegDst=%d, ALUSrc=%d, ALUOp=%d", r.RegDst, r.ALUSrc, r.ALUOp)
+	fmt.Printf("MemRead=%d, MemWrite=%d, Branch=%d", r.MemRead, r.MemWrite, r.Branch)
+	fmt.Printf("MemToReg=%d, RegWrite=%d, [%s] \n", r.MemToReg, r.RegWrite, r.Instr_String)
+
+	fmt.Printf("IncrPC= %d  ReadReg1Value=%X  ReadReg2Value=%X", r.Incr_PC, r.ReadReg1Value, r.ReadReg2Value)
+	fmt.Printf("SEOffset=%X  WriteReg_20_16=%X  WriteReg_15_11=%X  Function=%d", r.SEOffset, r.WriteReg_20_16, r.WriteReg_15_11, r.Function)
+}
+
+type EX_MEM_Base struct {
+	MemRead      bool
+	MemWrite     bool
+	Branch       bool
+	MemToReg     bool
+	RegWrite     bool
+	CalcBTA      int
+	Zero         bool
+	ALUResult    int
+	SWValue      int
+	WriteRegNum  int
+	Reg_Type     string
+	Instr_String string
 }
 
 type EX_MEM_Write struct {
-	MemRead     bool
-	MemWrite    bool
-	Branch      bool
-	MemToReg    bool
-	RegWrite    bool
-	CalcBTA     int
-	Zero        bool
-	ALUResult   int
-	SWValue     int
-	WriteRegNum int
+	EX_MEM_Base
 }
 
 type EX_MEM_Read struct {
-	MemRead     bool
-	MemWrite    bool
-	Branch      bool
-	MemToReg    bool
-	RegWrite    bool
-	CalcBTA     int
-	Zero        bool
-	ALUResult   int
-	SWValue     int
-	WriteRegNum int
+	EX_MEM_Base
+}
+
+func (r *EX_MEM_Base) dump_EX_MEM() {
+	fmt.Printf("\n")
+	fmt.Printf("EX / MEM %s \n", r.Reg_Type)
+	fmt.Println("------------")
+
+	fmt.Printf("Control: \n")
+	fmt.Printf("MemRead=%d, MemWrite=%d, Branch=%d", r.MemRead, r.MemWrite, r.Branch)
+	fmt.Printf("MemToReg=%d, RegWrite=%d, [%s] \n", r.MemToReg, r.RegWrite, r.Instr_String)
+
+	fmt.Printf("CalcBTA=%X  Zero=%b  ALUResult=%X", r.CalcBTA, r.Zero, r.ALUResult)
+	fmt.Printf("SWValue=%X  WriteRegNum=%d", r.SWValue, r.WriteRegNum)
+}
+
+type MEM_WB_Base struct {
+	MemToReg     bool
+	RegWrite     bool
+	LWDataValue  int
+	ALUResult    int
+	WriteRegNum  int
+	Reg_Type     string
+	Instr_String string
 }
 
 type MEM_WB_Write struct {
-	MemToReg    bool
-	RegWrite    bool
-	LWDataValue int
-	ALUResult   int
-	WriteRegNum int
+	MEM_WB_Base
 }
 
 type MEM_WB_Read struct {
-	MemToReg    bool
-	RegWrite    bool
-	LWDataValue int
-	ALUResult   int
-	WriteRegNum int
+	MEM_WB_Base
+}
+
+func (r *MEM_WB_Base) dump_MEM_WB() {
+	fmt.Printf("\n")
+	fmt.Printf("MEM / WB %s \n", r.Reg_Type)
+	fmt.Println("------------")
+
+	fmt.Printf("Control: \n")
+	fmt.Printf("MemToReg=%d, RegWrite=%d, [%s] \n", r.MemToReg, r.RegWrite, r.Instr_String)
+
+	fmt.Printf("LWDataValue=%X  ALUResult=%X  WriteRegNum=%d", r.LWDataValue, r.ALUResult, r.WriteRegNum)
 }
 
 func main() {
@@ -185,17 +219,21 @@ func main() {
 	// fmt.Printf("Main_Mem[0x101]=[%X]\n", Main_Mem[0x101])
 	// fmt.Printf("Registers: [%X]\n", Regs)
 
-	Print_out_everything()
+	// dump at Clock-Cycle 0
+	Print_out_everything(false)
 
-	for pc, instruction := range instructions {
-		clock_cyle++
-		IF_stage(pc, instruction)
+	for !exit_requested {
+		IF_stage()
 		ID_stage()
 		EX_stage()
 		MEM_stage()
 		WB_stage()
-		Print_out_everything()
+		Print_out_everything(false)
 		Copy_write_to_read()
+
+		// check write pipeline registers
+		// TODO: remove this call, and remove param from signature
+		Print_out_everything(true)
 	}
 }
 
@@ -220,22 +258,30 @@ func Initialize_Pipeline() {
 	ifid_r.Reg_Type = "Read"
 }
 
-func IF_stage(pc int, instruction int) {
-	showVerbose := false
-	// handle opcode format
-	if ((instruction & OPCODE_MASK) >> 26) == RFORMAT {
-		fetched_inst := Do_RFormat(instruction, showVerbose)
-		ifid_w.Instruction = fetched_inst.instruction
-		ifid_w.Incr_PC = pc
-		ifid_w.Instruction_String = fetched_inst.inst_string
-	} else {
-		// fetched_inst := Do_IFormat(instruction, showVerbose)
-	}
+func IF_stage() {
+	// Fetch Instruction
+	clock_cyle++
+	fetched_inst := instructions[pc]
+	pc++
 
+	// Copy to IF/ID Write pipeline register
+	ifid_w.Instruction = fetched_inst
+	ifid_w.Incr_PC = pc
 }
 
 func ID_stage() {
+	showVerbose := false
 
+	// Read instruction from IF/ID Read pipeline
+	instruction := ifid_r.Instruction
+
+	// handle opcode format
+	if ((instruction & OPCODE_MASK) >> 26) == RFORMAT {
+		decoded_inst := Do_RFormat(instruction, showVerbose)
+
+	} else {
+		decoded_inst := Do_IFormat(instruction, showVerbose)
+	}
 }
 
 func EX_stage() {
@@ -250,18 +296,29 @@ func WB_stage() {
 
 }
 
-func Print_out_everything() {
-	fmt.Printf("Clock Cycle %d \n", clock_cyle)
+func Print_out_everything(isAfterCopy bool) {
+	copyFlag := "Before"
+	if isAfterCopy {
+		copyFlag = "After"
+	}
+
+	fmt.Println("\n")
+	fmt.Printf("Clock Cycle %d [%s]\n", clock_cyle, copyFlag)
 	fmt.Println("------------------------------")
 	ifid_w.dump_IF_ID()
+	ifid_r.dump_IF_ID()
 
-	fmt.Println("         --- END ---          ")
+	fmt.Println("         --- END ---          \n")
 }
 
 func Copy_write_to_read() {
-
+	CopyIFID()
 }
 
+func CopyIFID() {
+	ifid_r.Instruction = ifid_w.Instruction
+	ifid_r.Incr_PC = ifid_r.Incr_PC
+}
 func Do_RFormat(instruction int, showVerbose bool) *R_Inst {
 	opcode := (instruction & OPCODE_MASK) >> 26
 	rs := (instruction & RS_MASK) >> 21
@@ -281,7 +338,7 @@ func Do_RFormat(instruction int, showVerbose bool) *R_Inst {
 		fmt.Println("---END--")
 	}
 
-	inst_string := fmt.Sprintf("[%3s  $%d, $%d, $%d]", func_code, rd, rs, rt)
+	inst_string := fmt.Sprintf("%3s  $%d, $%d, $%d", func_code, rd, rs, rt)
 	// fmt.Println(inst_string)
 
 	r := new(R_Inst)
@@ -315,8 +372,9 @@ func Do_IFormat(instruction int, showVerbose bool) *I_Inst {
 		fmt.Println("---END---")
 	}
 
-	inst_string := fmt.Sprintf("[%3s  $%d, %d($%d)]", op, rt, offset, rs)
+	inst_string := fmt.Sprintf("%3s  $%d, %d($%d)", op, rt, offset, rs)
 
+	// TODO: remove this before handing in assignment
 	// if op == "lb" || op == "sb" {
 	// fmt.Println(inst_string)
 	// } else {
